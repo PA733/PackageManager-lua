582cefcab9501b83737ee3c61ef735c6f63be7f7
RedbeanW <redbeana44945@gmail.com>
2022-07-09 17:08:49 +0800

    try to fix `cloud.lua`

--- share/cloud.lua
+++ share/cloud.lua
@@ -15,7 +15,7 @@ local Log = Logger:new('Cloud')
 local SizeConv = {
     Byte2Mb = function (num,saveBit)
         saveBit = saveBit or 2
-        return tonumber(string.format('%.'..saveBit..'f', num/1048576))
+        return tonumber(('%.'..saveBit..'f'):format(num/1048576))
     end
 }
 
@@ -28,9 +28,15 @@ Cloud = {
             prefix = { 'lanzou://' },
             servers = {
                 'lanzoui.com',
-                'lanzouf.com',
+                'lanzouj.com',
+                'lanzoul.com',
+                'lanzoup.com',
+                'lanzouq.com',
+                'lanzout.com',
+                'lanzouv.com',
+                'lanzouw.com',
                 'lanzoux.com',
-                'lanzous.com'
+                'lanzouy.com',
             }
         }
         --- ['Ftp'] = {
@@ -40,23 +46,27 @@ Cloud = {
     UA = "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36"
 }
 
-local function fetch(url)
-    for key,value in pairs(Cloud.Protocol) do
-        if type(value) == 'table' then
-            for n,prefix in pairs(value.prefix) do
-                if string.sub(url,1,string.len(prefix)) == prefix then
-                    return key,value
+---解析链接, 返回protocol名称
+---@param url string 链接
+---@return string|nil
+function Cloud:parseLink(url)
+    for name,protoObj in pairs(Cloud.Protocol) do
+        if type(protoObj) == 'table' then
+            for n,prefix in pairs(protoObj.prefix) do
+                if url:sub(1,prefix:len()) == prefix then
+                    return name
                 end
             end
         end
     end
-    Log:Error('正在解析无法识别的URL：%s',url)
     return nil
 end
 
-function Cloud.Protocol:getAll()
+---获取所有支持的协议
+---@return table
+function Cloud:getAllProtocol()
     local rtn = {}
-    for k,v in pairs(self) do
+    for k,v in pairs(self.Protocol) do
         if type(v) == 'table' then
             rtn[#rtn+1] = k
         end
@@ -65,19 +75,20 @@ function Cloud.Protocol:getAll()
 end
 
 --- 创建新任务
+---
+--- **HTTP** `https://hengaaaa.ugly/114.zip`
+---
+--- **LANZOU** `lanzou://tiansuohao2:pwd=humo`
+---
 ---@param dict table 需提供 url, writefunction, 可选 ua, header。
 ---@return any
 function Cloud:NewTask(dict)
-    --- URL:
-    --- (Http) https://example.com/a.zip
-    --- (Lanzou) lanzou://iv04c0128t9a:pwd=af1d
-    --- payload:
-    --- ua, header, writefunction
-    local name,protocol = fetch(dict.url)
-    if not protocol then
+    local name = self:parseLink(dict.url)
+    if not name then
+        Log:Error('正在解析无法识别的URL：%s',dict.url)
         return false
     end
-    local rtn
+    local protocol = self.Protocol[name]
     if dict.payload then
         for k,v in pairs(dict.payload) do
             dict.k = v
@@ -85,31 +96,28 @@ function Cloud:NewTask(dict)
         dict.payload = nil
     end
     if name == 'Http' then
-        rtn = protocol:get(dict.url,dict)
+        return protocol:get(dict.url,dict)
     elseif name == 'Lanzou' then
-        local tmp = string.split(dict.url,':')
-        local shareId = string.sub(tmp[2],3)
-        if string.sub(shareId,-1) == '/' then
-            shareId = string.sub(shareId,1,string.len(shareId)-1)
+        local tmp = dict.url:split(':')
+        local shareId = tmp[2]:sub(3)
+        if shareId:sub(-1) == '/' then
+            shareId = shareId:sub(1,shareId:len()-1)
         end
         local passwd
         if tmp[3] then
-            passwd = string.split(tmp[3],'=')
+            passwd = tmp[3]:split('=')
             if passwd then passwd = passwd[2] end 
         end
-        rtn = protocol:get(shareId,passwd,dict)
-    else
-        Log:Error('正在解析无法识别的URL：%s',dict.url)
+        return protocol:get(shareId,passwd,dict)
     end
-    return rtn
 end
 
 --- 蓝奏云解析下载
 ---
 --- *注意* 只支持单文件解析，目录解析暂不支持
 ---
----@param shareId string 分享ID, 即分享链接末部分=
----@param passwd string 密码(如果有), 可以为nil
+---@param shareId string 分享ID, 即分享链接末部分
+---@param passwd? string 密码(如果有), 可以为nil
 ---@param payload table 请求载荷
 function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
     local L = Logger:new('Lanzou')
@@ -138,8 +146,8 @@ function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
     for tryingNum,link in pairs(self.servers) do
 
         --- Init.
-        local baseUrl = string.format('https://www.%s/tp/',link)
-        local url = string.format('%s%s',baseUrl,shareId)
+        local baseUrl = ('https://www.%s/tp/'):format(link)
+        local url = ('%s%s'):format(baseUrl,shareId)
         local data = ''
 
         --- Get lanzou page, get informations.
@@ -164,18 +172,18 @@ function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
         --- Check page(server) status.
         if page_rtncode == 200 then
 
-            if string.find(data,'文件取消分享了') then
+            if data:find('文件取消分享了') then
                 L:Error('该文件分享链接已失效')
                 return false
             end
 
-            if string.find(data,'pwd') then
+            if data:find('pwd') then
                 if not passwd then
                     L:Error('提取码错误')
                     return false
                 end
                 --- Get file over passcode.
-                local sign = string.match(data,'action=downprocess&sign=([^\"]*)&p=')
+                local sign = data:match('action=downprocess&sign=([^\"]*)&p=')
                 if not sign then
                     L:Error('Sign获取失败')
                     return false
@@ -186,11 +194,11 @@ function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
                 form:add_content('p',passwd)
                 local response = ''
                 local ajaxm = curl.easy {
-                    url = string.format('%sajaxm.php',baseUrl),
+                    url = ('%sajaxm.php'):format(baseUrl),
                     httpheader = {
                         'Accept: application/json, text/javascript, */*',
                         'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-                        string.format('Referer: %s%s',baseUrl,shareId),
+                        ('Referer: %s%s'):format(baseUrl,shareId),
                         'X-Requested-With: XMLHttpRequest'
 
                     },
@@ -215,7 +223,9 @@ function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
                 end
                 local rtn = JSON.parse(response)
                 if rtn and rtn.zt == 1 then
-                    local rtnCode,downUrl = getRedirect(string.format('%s/file/%s',rtn.dom,rtn.url))
+                    local _,downUrl = getRedirect(('%s/file/%s'):format(rtn.dom,rtn.url))
+                    print(downUrl)
+                    
                     return Cloud:NewTask {
                         url = downUrl,
                         payload = payload
@@ -226,19 +236,17 @@ function Cloud.Protocol.Lanzou:get(shareId,passwd,payload)
                 end
                 break
             else
-                --- Get file direct.
+                --- Get file download link.
                 local downlink,fileId
                 local lzReq = pcall(function()
-                    --- file_name = string.match(data,'<div class="md">([^\"]*) <span class="mtt">')
-                    --- release_date = string.match(data,'<span class="mt2">时间:</span>([^\"]*) <span')
-                    --- uploader_name = string.match(data,'<span class="mt2">发布者:</span>([^\"]*) <span')
-                    downlink = string.match(data,'href = \'([^\"]*)\' +')
-                    assert(downlink)
-                    fileId = string.match(data,'var loaddown = \'([^\"]*)\';')
-                    fileId = string.match(fileId,'([^\"]*)\';')
+                    data = data:gsub('\n',''):gsub(' ','')
+                    -- print(data)
+                    downlink = data:match(";varpototo='([^\"]*)';varspototo")
+                    fileId = data:match(";varspototo='([^\"]*)';//vardomianload")
+                    assert(downlink and fileId)
                 end)
                 if not lzReq then
-                    L:Error('解析失败')
+                    L:Error('解析失败，请检查模块更新。')
                     return false
                 end
                 local redict_rtncode,final_link = getRedirect(downlink..fileId)
@@ -266,7 +274,8 @@ end
 ---@param url string 链接
 ---@param payload table 请求载荷
 function Cloud.Protocol.Http:get(url,payload)
-    Log:Debug('下载: %s',url)
+    -- Log:Debug('下载: %s',url)
+    local blocks = 40
     local proInfo = {
         recording = {},
         call_times = 0,
@@ -274,7 +283,7 @@ function Cloud.Protocol.Http:get(url,payload)
         max_size = 0,
         steps = { '○','◔','◑','◕','●' },
         step = 1,
-        progress = ('━'):rep(20),
+        progress = ('━'):rep(blocks),
         size_vaild = false,
         completed = false
     }
@@ -304,7 +313,7 @@ function Cloud.Protocol.Http:get(url,payload)
                 --- calc progress
                 if size ~= 0 then
                     Rec.size_vaild = true
-                    Rec.progress = string.gsub(string.rep('—',20),'—','━',math.floor(20*(downloaded/size)))
+                    Rec.progress = ('—'):rep(blocks):gsub('—','━',math.floor(blocks*(downloaded/size)))
                 end
             end
             Rec.recording[time] = downloaded
@@ -315,12 +324,12 @@ function Cloud.Protocol.Http:get(url,payload)
                     Rec.step = 1
                 end
             end
-            local formatted = string.format(' %s Downloading %s %sM/s (%sM/%sM)',Rec.steps[Rec.step],Rec.progress,Rec.average_speed,SizeConv.Byte2Mb(downloaded),SizeConv.Byte2Mb(size))
-            local strlen = string.len(formatted)
+            local formatted = (' %s Downloading %s %sM/s (%sM/%sM)'):format(Rec.steps[Rec.step],Rec.progress,Rec.average_speed,SizeConv.Byte2Mb(downloaded),SizeConv.Byte2Mb(size))
+            local strlen = formatted:len()
             if Rec.max_size < strlen then
                 Rec.max_size = strlen
             end
-            io.write('\r',formatted,string.rep(' ',Rec.max_size - strlen))
+            --io.write('\r',formatted,(' '):rep(Rec.max_size - strlen))
         end,
         noprogress = false,
         ssl_verifypeer = false,
@@ -328,7 +337,7 @@ function Cloud.Protocol.Http:get(url,payload)
         timeout = 15
     }
     local msf = easy:perform()
-    io.write(string.format('\r √ Completed, %s %sM/s (%sM).',string.rep('━',20),SizeConv.Byte2Mb(msf:getinfo_speed_download()),SizeConv.Byte2Mb(msf:getinfo_size_download()))..string.rep(' ',8),'\n')
+    --io.write(('\r √ Completed, %s %sM/s (%sM).'):format(('━'):rep(blocks),SizeConv.Byte2Mb(msf:getinfo_speed_download()),SizeConv.Byte2Mb(msf:getinfo_size_download()))..(' '):rep(8),'\n')
     easy:close()
     return true
 end
