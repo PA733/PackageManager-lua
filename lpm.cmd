4b891bce02677e8ddbe37252d2f5258c63d1a1b1
Redbeanw44602 <redbeana44945@gmail.com>
2023-01-07 18:17:22 +0800

    Revert "Add runtime and lpm.cmd"
    This reverts commit 3074923a2915c9c78f69413a01472e71b6f0a081.

--- lpm.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-REM Add ./lib and ./runtime/lua-5.4.2_Win64_bin to PATH
-
-SET PATH=%PATH%;%~dp0lib;%~dp0runtime\lua-5.4.2_Win64_bin
-
-REM Run Run.lua with the arguments passed to this script
-
-lua54 %~dp0Run.lua %*
